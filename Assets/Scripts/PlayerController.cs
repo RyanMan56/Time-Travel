1aaa17cef830eeb591b171cdb8a9e590e9a94efb
Ryan Jones <ryan.jones@wearenova.co.uk>
2018-05-31 17:21:48 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Time.delta doesn't need to be used for FixedUpdate, since it happens at a fixed time step
+	void FixedUpdate () {
+	    
+	}
+}
